f09ceb61d56b680321e08a7ce1ae2821767cf7ed
changjianwang <changjianwang@cyou-inc.com>
2016-08-05 11:26:55 +0800

    数组语法

--- src/module/demo10/demo10.js
+++ src/module/demo10/demo10.js
@@ -7,7 +7,16 @@ var vm = new Vue({
     fontSize: 30,
     styleObject: {
       color: 'blue',
-      fontSize: '50px'
+      fontSize: '50px',
+      'text-align': 'left'
+    },
+    styleObjectA: {
+      color: 'yellow'
+    },
+    styleObjectB: {
+      fontSize: '50px',
+      'text-align': '-webkit-left',
+      transitionDelay: 'inherit'    //当 v-bind:style 使用需要厂商前缀的 CSS 属性时，如 transform，Vue.js 会自动侦测并添加相应的前缀。
     }
   }
 })
