d9cacebabc2baa9d8f6f079580319b63110bfb08
emila <10877098@qq.com>
2016-08-10 12:33:06 +0800

    自定义事件

--- src/module/demo19/demo19.js
+++ src/module/demo19/demo19.js
@@ -1,41 +1,34 @@
 var Vue = require('vue')
-// 定义
+// 注册子组件
+// 将当前消息派发出去
 Vue.component('child', {
-  // camelCase in JavaScript
-//  props: ['myMessage'],
-  props: {
-    myMessage: {
-      validator: function (value) {
-        return parseInt(value, 10) > 10
-      },
-      type: String
-    }
+  template: '#child-template',
+  data: function () {
+    return { msg: 'hello' }
   },
-  template: '<span>{{ myMessage }}</span>'
-})
-
-
-
-
-// 创建根实例
-new Vue({
-  el: '#example',
-  data: {
-    parentMsg: '15'
+  methods: {
+    notify: function () {
+      if (this.msg.trim()) {
+        this.$dispatch('child-msg', this.msg)
+        this.msg = ''
+      }
+    }
   }
 })
 
-
-//new Vue({
-//  el: '#example',
-//  data: {
-//    parentMsg: 'Message from parent'
-//  },
-//  components: {
-//    child: {
-//      props: ['myMessage'],
-//      template: '<span>{{myMessage}}</span>'
-//    }
-//  }
-//})
+// 初始化父组件
+// 将收到消息时将事件推入一个数组
+var parent = new Vue({
+  el: '#events-example',
+  data: {
+    messages: []
+  },
+  // 在创建实例时 `events` 选项简单地调用 `$on`
+  events: {
+    'child-msg': function (msg) {
+      // 事件回调内的 `this` 自动绑定到注册它的实例上
+      this.messages.push(msg)
+    }
+  }
+})
 
